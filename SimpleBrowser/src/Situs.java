879570abdf917ca0ecabf7684be24df7946d6430
Renth <135339655+Arch-Ren@users.noreply.github.com>
2026-09-17 15:14:32 +0700

    feat site

---
+++ SimpleBrowser/src/Situs.java
@@ -0,0 +1,9 @@
+public class Situs {
+    String alamatSitus;
+    String namaSitus;
+
+    Situs(String url, String title) {
+        this.alamatSitus = url;
+        this.namaSitus = title;
+    }
+}
